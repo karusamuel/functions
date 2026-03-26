c1551ea0cf9c13f350618ba3695be9b9484aa21b
samuel karu <karusamuelngugi@gmail.com>
2026-03-26 14:40:05 +0300

    added regular exp

---
+++ greet/__init__.py
@@ -0,0 +1,4 @@
+from .farewell import say_goodbye
+from .greetings import say_hello
+# import greetings
+# import farewell
---
+++ math_tools.py
@@ -0,0 +1,11 @@
+# add two numbers 
+answer = 23
+def add(a,b):
+    answer=a+b
+    return(a+b)
+
+#multiplys two numbers 
+def multi(a,b):
+    return (a*b)
+
+# add a function that return the remaider 
